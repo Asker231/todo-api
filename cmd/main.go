c9b77db9cbc6ec332951764018564d8ca9f32807
Asker <kotsevasker0@gmail.com>
2025-01-24 18:54:51 +0300

    add new pkg

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 
 
 func main(){
+	//create router
+	router := http.NewServeMux()
+
+	//create server
+	server := http.Server{
+		Addr: ":8080",
+		Handler: router,
+	}
 	
+	//server started
+	err := server.ListenAndServe()
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+	fmt.Println("Server started http://localhost:8080/")
 }
